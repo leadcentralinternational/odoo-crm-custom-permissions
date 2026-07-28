ed1d61999a78520c24f4a9c24dc9a8054a846abb
hannsksa <hannssa@gmail.com>
2026-07-27 22:42:41 -0500

    refactor: simplify _filter_visible_menus to prevent menu hierarchy corruption

--- models/ir_ui_menu.py
+++ models/ir_ui_menu.py
@@ -5,22 +5,18 @@ class IrUiMenu(models.Model):
     _inherit = 'ir.ui.menu'
 
     def _filter_visible_menus(self):
-        if self.env.context.get('skip_custom_crm_filter'):
-            return super(IrUiMenu, self)._filter_visible_menus()
-
+        visible_menus = super(IrUiMenu, self)._filter_visible_menus()
         user = self.env.user
-        
-        # System administrators bypass custom menu restrictions
+
+        # Apply custom permissions for non-admin users with custom permissions enabled
         if user.crm_custom_permissions_enabled and not user.has_group('base.group_system'):
             sales_app_menu = self.env.ref('sale.sale_menu_root', raise_if_not_found=False)
-            crm_app_menu = self.env.ref('crm.crm_menu_root', raise_if_not_found=False)
-            
             crm_sales_menu = self.env.ref('crm.crm_menu_sales', raise_if_not_found=False)
             crm_report_menu = self.env.ref('crm.crm_menu_report', raise_if_not_found=False)
             crm_config_menu = self.env.ref('crm.crm_menu_config', raise_if_not_found=False)
 
             exclude_menus = self.env['ir.ui.menu']
-            
+
             # 1. Always hide Sales App root menu
             if sales_app_menu:
                 exclude_menus |= sales_app_menu
@@ -34,40 +30,21 @@ class IrUiMenu(models.Model):
                 if crm_config_menu and not user.crm_show_menu_config:
                     exclude_menus |= crm_config_menu
             else:
-                # If Admin Options is not checked, hide Configuration menu by default for non-admins
+                # If Admin Options is disabled, hide Configuration menu by default
                 if crm_config_menu:
                     exclude_menus |= crm_config_menu
 
-            def is_excluded(menu):
-                current = menu
-                while current:
-                    if current in exclude_menus:
-                        return True
-                    current = current.parent_id
-                return False
+            if exclude_menus:
+                def is_excluded(menu):
+                    current = menu
+                    while current:
+                        if current in exclude_menus:
+                            return True
+                        current = current.parent_id
+                    return False
 
-            menu_env = self.with_context(skip_custom_crm_filter=True)
+                visible_menus = visible_menus.filtered(lambda m: not is_excluded(m))
 
-            candidate_menus = self
-            if crm_app_menu:
-                crm_branch = menu_env.search([('id', 'child_of', crm_app_menu.id)])
-                candidate_menus = candidate_menus | crm_branch
-
-            visible_menus = super(IrUiMenu, candidate_menus)._filter_visible_menus()
-
-            # Re-include CRM Configuration branch if explicitly allowed in Admin Options
-            if crm_app_menu and user.crm_admin_options_enabled and user.crm_show_menu_config and crm_config_menu:
-                config_branch = menu_env.search([('id', 'child_of', crm_config_menu.id)])
-                visible_menus |= config_branch
-
-            # Always ensure CRM root app menu is visible if custom permissions are enabled
-            if crm_app_menu:
-                visible_menus |= crm_app_menu
-
-            # Filter out excluded menus (Sales App, disabled CRM submenus)
-            visible_menus = visible_menus.filtered(lambda m: not is_excluded(m))
-            return visible_menus
-
-        return super(IrUiMenu, self)._filter_visible_menus()
+        return visible_menus
 
 
